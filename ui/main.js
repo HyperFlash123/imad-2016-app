4f50b0fd0ffe32fc8a876a19f340df64f30ec7e7
HyperFlash123 <vlavanya418@gmail.com>
2016-11-30 02:46:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,29 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://hyperflash123.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
 
-var element=document.getElementById('main.text');
-
-element.innerHTML='New value';
-
-var img=documnet.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
 };
