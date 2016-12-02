6f992b4443d768ef62c781447f8a298a4c789c4a
HyperFlash123 <vlavanya418@gmail.com>
2016-12-02 18:58:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,18 @@
-/*var button=document.getElementById('counter');
+/*console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHTML='New value';
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveR(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveR,100);
+};
+
+*/
+var button=document.getElementById('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
@@ -14,29 +28,25 @@ button.onclick=function(){
     request.send(null);
 };
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-    
-};*/
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+               var names=request.responseText;
+               names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+};
 
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveR(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveR,100);
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+request.open('GET','http://hyperflash123.imad.hasura-app.io/submit-name?name='+name,true);
 };
