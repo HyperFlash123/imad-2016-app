8d2a86ca7b43ad1409e930f4a6009d841c34e114
HyperFlash123 <vlavanya418@gmail.com>
2016-12-02 18:58:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,13 +76,13 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-/*var names=[];
-app.get('/submit-none',function(req,res){
-    var name=req.query.name;
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name=req.qyery.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
-*/
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;  
   res.send(createTemplate(articles[articleName]));
