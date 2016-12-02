15f7ec22a559e16cd8e8e19e6931dfaab7b58e3d
HyperFlash123 <vlavanya418@gmail.com>
2016-12-02 23:24:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'hyperflash123',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-hyperflash123-32058'
 };
 
 var app = express();
